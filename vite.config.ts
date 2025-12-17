1969e6591e197e9b4f729a7a4768663195134373
hombresenforma <ivan.hombresenforma@gmail.com>
2025-12-17 10:56:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hombresenforma_app/',
+  // Usar './' asegura que la app funcione sea cual sea el nombre de tu repositorio
+  base: './',
 })
